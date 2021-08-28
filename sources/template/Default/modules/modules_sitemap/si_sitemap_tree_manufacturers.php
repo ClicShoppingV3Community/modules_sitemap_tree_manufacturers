70c432ce25a3bedc4079b1476c46ca0246370f6e
ClicShopping <admin@clicshopping.org>
2021-08-28 14:43:42 +0200

    ADD TYPAGE

--- sources/template/Default/modules/modules_sitemap/si_sitemap_tree_manufacturers.php
+++ sources/template/Default/modules/modules_sitemap/si_sitemap_tree_manufacturers.php
@@ -16,8 +16,8 @@
   class si_sitemap_tree_manufacturers {
     public string $code;
     public string $group;
-    public string $title;
-    public string $description;
+    public $title;
+    public $description;
     public ?int $sort_order = 0;
     public bool $enabled = false;
 
